fbd7b008fba8011d48de4d699aa6d4eff192be01
JiangJunjie <jiangjunjie@megvii.com>
2017-09-13 10:53:13 +0800

    update

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-var path = require('path'),
+let path = require('path'),
 	fs = require('fs'),
 	cheerio = require('cheerio');
 
-function handle (src, dest) {
+function handle ({src, dest, cssDest = path.posix.join(dest,'css'), jsDest = path.posix.join(dest,'js')}) {
+	console.log(src,dest,cssDest,jsDest);
 	if (!Array.isArray(src)) {
 		src = [src];
 	}
 
-	for (var i = 0; i < src.length; i++) {
-		var srcFile = src[i], content = fs.readFileSync(srcFile, 'utf-8'), $ = cheerio.load(content);
+	for (let i = 0; i < src.length; i++) {
+		let srcFile = src[i], content = fs.readFileSync(srcFile, 'utf-8'), $ = cheerio.load(content);
 
-		var destCssName = path.posix.basename(srcFile).replace(/\.([^.]+)$/, ".css"), 
-			destCssPath = path.posix.join(dest, destCssName),
-			$links = $("link");
+		let destCssName = path.posix.basename(srcFile).replace(/\.([^.]+)$/, ".css"), 
+			destCssPath = path.posix.join(cssDest, destCssName),
+			destJsName = path.posix.basename(srcFile).replace(/\.([^.]+)$/, ".js"), 
+			destJsPath = path.posix.join(jsDest, destJsName),
+			$links = $("link"), $scipts = $("script");
 
 		if ($links.length > 0) {
 			fs.existsSync(destCssPath) && fs.unlinkSync(destCssPath);
 			fs.writeFileSync(destCssPath);
-			var w = fs.createWriteStream(destCssPath);
+			let w = fs.createWriteStream(destCssPath);
 			
-			$("link").filter(function(i, ele){
+			$links.filter(function(i, ele){
 				return $(this).attr("href") && $(this).attr("href").endsWith(".css"); 
 			}).each(function(i, elem) {
-
-		  		var href = $(this).attr("href");
+		  		let href = $(this).attr("href");
 		  			r = fs.createReadStream(path.posix.join(path.posix.dirname(srcFile),href));
 		  		console.log(path.posix.join(path.posix.dirname(srcFile),href));
 		  		r.pipe(w);
 			});
 		}
+
+		if ($scipts.length) {
+			fs.existsSync(destJsPath) && fs.unlinkSync(destJsPath);
+			fs.writeFileSync(destJsPath);
+			let w = fs.createWriteStream(destJsPath);
+
+			$scipts.filter(function(i, ele){
+				return $(this).attr("src") && $(this).attr("src").endsWith(".js"); 
+			}).each(function(i, elem) {
+		  		let src = $(this).attr("src");
+		  			r = fs.createReadStream(path.posix.join(path.posix.dirname(srcFile),src));
+		  		console.log(path.posix.join(path.posix.dirname(srcFile),src));
+		  		r.pipe(w);
+			});
+		}
+
+		
 	}
 }
 
-handle('../labelpp-frontend/page/landing.html', '../labelpp-frontend/dist/css');
+handle({src:'../labelpp-frontend/page/landing.html', dest:'../labelpp-frontend/dist'});
 
 
 exports = function (src, dest) {
